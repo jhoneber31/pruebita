6dc929f654738ffee6b63859bddfdf570fd044f5
Jhon Eber <jhoneber31@gmail.com>
2023-09-02 09:05:06 -0500

    WAzA

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-          wwa
+          wwazaaa
         >
           Learn React
         </a>
